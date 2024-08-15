d75f06d2e98c25f73fc04752a5c9e6fdd8470fa7
Emdinga <emdinga@gmail.com>
2024-08-15 14:22:12 +0200

    db fix

--- app.py
+++ app.py
@@ -34,4 +34,6 @@ def watch_stream(stream_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)

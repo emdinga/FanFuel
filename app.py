fba9139d3601570411af7aaf3cb34e722e41e9bb
Emdinga <emdinga@gmail.com>
2024-08-15 11:49:40 +0200

    my fisrt commit of the application

---
+++ app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Flask application for live streams"""
+
+
+from models import Stream
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Query for active streams"""
+    streams = Stream.query.filter_by(is_live=True).all()
+    return render_template('home.html', streams=streams)
+
+@app.route('/watch/<int:stream_id>')
+def watch_stream(stream_id):
+    """display live streams"""
+    stream = Stream.query.get_or_404(stream_id)
+    return render_template('watch_stream.html', stream=stream)
---
+++ models.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from app import db
+
+class Stream(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    streamer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    is_live = db.Column(db.Boolean, default=False)
+    viewers_count = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)

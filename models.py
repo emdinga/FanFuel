f1032c068cca9d3e1100514bb2092c084c6e7a3c
Emdinga <emdinga@gmail.com>
2024-08-16 09:03:57 +0200

    adding foriemgn relations

--- models.py
+++ models.py
@@ -3,15 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Stream(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    streamer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    is_live = db.Column(db.Boolean, default=False)
-    viewers_count = db.Column(db.Integer, default=0)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -20,6 +11,19 @@ class Profile(db.Model):
     picture = db.Column(db.String(200), nullable=True, default='default.jpg')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     status = db.Column(db.String(10), nullable=False, default='offline')
-    streams = db.relationship('Stream', backref='streamer', lazy=True)
+    streams = db.relationship('Stream', backref='profile', lazy=True)
+    
     def __repr__(self):
         return f'<Profile {self.username}>'
+
+class Stream(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    streamer_id = db.Column(db.Integer, db.ForeignKey('profiles.id'), nullable=False)
+    is_live = db.Column(db.Boolean, default=False)
+    viewers_count = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f'<Stream {self.title}>'
